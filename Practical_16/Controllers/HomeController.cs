bfccd85329185310415f0675d578b77a9fb17f98
Trupal Kaila <trupalkaila8@gmail.com>
2025-12-15 18:13:19 +0530

    Add project files.

---
+++ Practical_16/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Practical_16.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+        public HomeController (ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult HelloWorld()
+        {
+            _logger.LogInformation("Hello World API is Executed!!");
+            return Ok("Hello World");
+        }
+    }
+}
